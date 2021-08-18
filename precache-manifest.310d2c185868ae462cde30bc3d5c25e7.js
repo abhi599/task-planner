93a517fc7200adbae90b929964dffebb5f8b7f4b
baba_yaga_ <36357951+baba_yaga_@users.noreply.github.com>
2021-08-18 10:12:00 +0530

    Updates

---
+++ precache-manifest.310d2c185868ae462cde30bc3d5c25e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3825283edcdf66d2a56d",
+    "url": "/task-planner/static/css/main.14873fef.chunk.css"
+  },
+  {
+    "revision": "3825283edcdf66d2a56d",
+    "url": "/task-planner/static/js/main.3825283e.chunk.js"
+  },
+  {
+    "revision": "a46c3e295f5ff88ce5c1",
+    "url": "/task-planner/static/js/1.a46c3e29.chunk.js"
+  },
+  {
+    "revision": "7c198d073e1fbcd33725",
+    "url": "/task-planner/static/js/runtime~main.7c198d07.js"
+  },
+  {
+    "revision": "bad4a54a1325493de97b17acd8ca18ae",
+    "url": "/task-planner/index.html"
+  }
+];
